d9e792ca0c3913462fbca63284c6a070ba9ae55e
marekzska <marekzska@gmail.com>
2024-08-28 13:20:02 +0200

    Added a favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navigation } from "./components/Navigation";
 import { Footer } from "./components/Footer";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
