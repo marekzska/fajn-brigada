cd0281412961384ed50a6256da7c12bb23f06676
marekzska <marekzska@gmail.com>
2024-09-23 15:10:42 +0200

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <Script
+        id='chatbot-config'
         dangerouslySetInnerHTML={{
           __html: `
             window.embeddedChatbotConfig = {
